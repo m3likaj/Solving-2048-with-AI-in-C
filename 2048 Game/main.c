f5354517c13bac78805ace4806c9898f6fa1c94d
Melika <m3likaj@gmail.com>
2025-01-17 20:05:51 +0300

    Initial Commit

---
+++ 2048 Game/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2048 Game
+//
+//  Created by MELIKA on 1/17/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
